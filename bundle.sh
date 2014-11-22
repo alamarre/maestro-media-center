4b7f37faf95f10fefb6fd4983e1f9b34527b3afe
Al Lamarre <alamarre@gmail.com>
2014-11-22 17:31:46 -0500

    use github releases

--- bundle.sh
+++ bundle.sh
@@ -1,7 +1,5 @@
-mkdir output
-
 cd maestro-server/target
-zip -r ../../output/maestro-$1.zip AllInOne-1.0.jar lib
+zip -r ../../maestro.zip AllInOne-1.0.jar lib
 cd ../.. 
 cd ui/omny-angular
-zip -r ../../output/maestro-$1.zip public_html
+zip -r ../../maestro.zip public_html
